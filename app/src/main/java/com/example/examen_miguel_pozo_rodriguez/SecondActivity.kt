b22f8710ef9667eb4d3e88f0a2b40ec1cfcdab22
miguelpozrod <miguel.pozrod.1@educa.jcyl.es>
2024-11-22 16:12:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/examen_miguel_pozo_rodriguez/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.examen_miguel_pozo_rodriguez
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+
+    }
